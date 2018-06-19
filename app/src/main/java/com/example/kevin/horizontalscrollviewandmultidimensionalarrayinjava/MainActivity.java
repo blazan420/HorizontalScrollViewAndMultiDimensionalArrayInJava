980d351941d1a3a9db2ff133b5ee90131831c814
kevin <kevinjobrien420@gmail.com>
2018-06-18 21:00:35 -0500

    209 App Number 22 - Part2 - Horizontal ScrollView and MultiDimensional Array in Java // Coding

--- app/src/main/java/com/example/kevin/horizontalscrollviewandmultidimensionalarrayinjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/horizontalscrollviewandmultidimensionalarrayinjava/MainActivity.java
@@ -18,10 +18,26 @@ public class MainActivity extends AppCompatActivity {
         int[][] intArray1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}, {13, 14, 15}, {16, 17, 18},
         {19, 20, 21}, {22, 23, 24}};
 
+        int[][] intArray2 = {{100, 101}, {102}, {103, 104, 105}, {106, 107, 108}, {109, 110, 111}, {112, 113, 114, 115, 116, 117},
+                {118, 119, 120}};
+
+        letsOutputTheArrayToTheScreen(intArray1, txtIntArray1);
+        letsOutputTheArrayToTheScreen(intArray2, txtIntArray2);
 
 
+    }
 
+    public void letsOutputTheArrayToTheScreen(int[][] intArray, TextView textView) {
 
+        String oldColumnsValue;
 
+        for (int row = 0; row < intArray.length; row++) {
+
+            for (int column = 0; column < intArray[row].length; column++) {
+
+                oldColumnsValue = textView.getText().toString();
+                textView.setText(oldColumnsValue + intArray[row][column] + " - " );
+            }
+        }
     }
 }
